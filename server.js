a0cdb58294fcb665a786beb9841f8e4254f15719
Qzifyes <Qzifyes>
2018-08-10 02:52:17 +0900

    add server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+﻿const connect = require('connect');
+const serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(3000, function(){
+    console.log('->port:3000');
+});
